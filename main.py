51fdebb293d8414669e24f3c651743e0bc9c5f65
Christopher Heje Grønbech <chgroenbech@gmail.com>
2017-02-15 17:14:11 +0100

    Add argument parser to main script

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import analysis
 
 import argparse
 
-def main(data_directory, log_directory, results_directory,
-    (splitting_method, splitting_fraction),
+from numpy import random
+
+random.seed(42)
+
+def main(data_set_name, data_directory, log_directory, results_directory,
+    splitting_method, splitting_fraction,
     latent_size, hidden_sizes, reconstruction_distribution,
     number_of_reconstruction_classes,
     number_of_warm_up_epochs,
@@ -22,24 +26,128 @@ def main(data_directory, log_directory, results_directory,
         splitting_method, splitting_fraction)
     
     feature_size = data_set.feature_size
-    
+
     # Modeling
-    
+
     model = modeling.VaritationalAutoEncoder(
         feature_size, latent_size, hidden_sizes,
         reconstruction_distribution, number_of_reconstruction_classes,
-        log_directory
     )
-    
+
+    log_directory += "/" + model.name
+
     model.train(training_set, validation_set,
         number_of_epochs, batch_size, learning_rate,
-        reset_training)
-    
+        log_directory, reset_training)
+
     # Analysis
-    
+
+    results_directory += "/" + model.name
+
     analysis.analyseModel(model, model.name, results_directory)
-    
+
     reconstructed_test_set, latent_set, test_metrics = model.evaluate(test_set)
-    
+
     analysis.analyseResults(test_set, reconstructed_test_set, latent_set,
         model.name, results_directory)
+
+parser = argparse.ArgumentParser(
+    description='Model single-cell transcript counts using deep learning.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument(
+    "--data-set-name",
+    type = str,
+    default = "mouse retina",
+    # default = "sample",
+    help = "name of data set"
+)
+parser.add_argument(
+    "--data-directory",
+    type = str,
+    default = "data",
+    help = "directory where data is placed"
+)
+parser.add_argument(
+    "--log-directory",
+    type = str,
+    default = "log",
+    help = "directory where models are logged"
+)
+parser.add_argument(
+    "--results-directory",
+    type = str,
+    default = "results",
+    help = "directory where results are saved"
+)
+parser.add_argument(
+    "--splitting-method",
+    type = str,
+    default = "random", 
+    help = "method for splitting data into training, validation, and test sets"
+)
+parser.add_argument(
+    "--splitting-fraction",
+    type = float,
+    default = 0.8,
+    help = "fraction to use when splitting data into training, validation, and test sets"
+)
+parser.add_argument(
+    "--latent-size",
+    type = int,
+    default = 50,
+    help = "size of latent space"
+)
+parser.add_argument(
+    "--hidden-sizes",
+    type = int,
+    nargs = '+',
+    default = [5000, 500],
+    help = "sizes of hidden layers"
+)
+parser.add_argument(
+    "--reconstruction-distribution",
+    type = str,
+    default = None,
+    help = "distribution for the reconstructions"
+)
+parser.add_argument(
+    "--number-of-reconstruction-classes",
+    type = int,
+    default = 0,
+    help = "the maximum count for which to use classification"
+)
+parser.add_argument(
+    "--number-of-epochs",
+    type = int,
+    default = 100,
+    help = "number of epochs for which to train"
+)
+parser.add_argument(
+    "--batch-size",
+    type = int,
+    default = 100,
+    help = "batch size used when training"
+)
+parser.add_argument(
+    "--learning-rate",
+    type = float,
+    default = 1e-3,
+    help = "learning rate when training"
+)
+parser.add_argument(
+    "--number-of-warm-up-epochs",
+    type = int,
+    default = 0,
+    help = "number of epochs with a linear weight on the KL-term"
+)
+parser.add_argument(
+    "--reset-training",
+    action = "store_true",
+    help = "reset already trained model"
+)
+
+if __name__ == '__main__':
+    arguments = parser.parse_args()
+    main(**vars(arguments))
