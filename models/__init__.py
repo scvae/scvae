be9fa5c23d56eaceee9814e23b9da53d5ac33cb6
maximillian91 <maxfvording@gmail.com>
2017-04-04 17:27:37 +0200

    Add ImportanceWeightedVariationalAutoEncoder to models.

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from models.variational_auto_encoder import VariationalAutoEncoder
+from models.importance_weighted_variational_auto_encoder import ImportanceWeightedVariationalAutoEncoder
 from models.simple_neural_network import SimpleNeuralNetwork
