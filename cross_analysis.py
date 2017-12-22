e0eeb01fea960b8bb0e8c07d75a2d0d1b562dd28
Christopher Heje Grønbech <chgroenbech@gmail.com>
2017-12-22 14:05:26 +0100

    Update cross analysis script

--- cross_analysis.py
+++ cross_analysis.py
@@ -5,27 +5,26 @@ import pickle
 import gzip
 import argparse
 
-from analysis import formatStatistics, plotCountLikelihoods, saveFigure
-from auxiliary import formatTime
+from analysis import formatStatistics
+from auxiliary import formatTime, title, subtitle
 
-test_metrics_filename = "test_metrics.pkl.gz"
+test_metrics_filename = "test-metrics.pkl.gz"
 
 def main(log_directory = None, results_directory = None,
-    name = "", y_scale = 'log', bar_plot = False, 
-    max_count_limit = None, min_count_limit = None,
     data_set_include_search_strings = [], 
     model_include_search_strings = [],
     data_set_exclude_search_strings = [], 
     model_exclude_search_strings = []):
     
-    count_limit_slice = slice(min_count_limit, max_count_limit + 1)
-
     if log_directory:
         log_directory = os.path.normpath(log_directory) + os.sep
 
     if results_directory:
+        
         results_directory = os.path.normpath(results_directory) + os.sep
+        
         test_metrics_set = testMetricsInResultsDirectory(results_directory)
+        
         for data_set, models in test_metrics_set.items():
             
             data_set_match = True
@@ -45,10 +44,8 @@ def main(log_directory = None, results_directory = None,
             if not data_set_match:
                 continue
             
-            print(data_set)
-            print()
-            count_likelihoods = []
-            count_distribution_names = []
+            title(data_set)
+            
             for model, test_metrics in models.items():
                 
                 model_match = True
@@ -67,8 +64,8 @@ def main(log_directory = None, results_directory = None,
                 
                 if not model_match:
                     continue
-            
-                print(model)
+                
+                subtitle(model)
                 
                 # Time
                 
@@ -128,23 +125,13 @@ def main(log_directory = None, results_directory = None,
                 
                 print()
 
-                # Count likelihoods
-
-                count_likelihoods.append(test_metrics["count likelihoods"])
-                count_distribution_names.append(test_metrics["count distribution name"])
-
-            # Plot count likelihood curves for all models
-            if len(count_likelihoods) > 0:
-                print("Plotting count likelihoods for found models")
-                figure, figure_name = plotCountLikelihoods(count_likelihoods, count_distribution_names, y_scale = y_scale, name = name, count_limit = count_limit_slice)
-                saveFigure(figure, figure_name, os.path.join(results_directory, data_set))
-
 def testMetricsInResultsDirectory(results_directory):
     
     test_metrics_set = {}
     
     for path, directories, files in os.walk(results_directory):
         if test_metrics_filename in files:
+            
             data_set_model = path.replace(results_directory, "")
             data_set_model_parts = data_set_model.split(os.sep)
             data_set = os.sep.join(data_set_model_parts[:3])
@@ -154,8 +141,10 @@ def testMetricsInResultsDirectory(results_directory):
                 test_metrics_set[data_set] = {}
             
             test_metrics_path = os.path.join(path, test_metrics_filename)
+            
             with gzip.open(test_metrics_path, "r") as test_metrics_file:
                 test_metrics_data = pickle.load(test_metrics_file)
+                
             test_metrics_set[data_set][model] = test_metrics_data
     
     return test_metrics_set
@@ -174,27 +163,6 @@ parser.add_argument(
     "--results-directory", "-R",
     type = str,
     help = "directory where results were saved"
-)
-parser.add_argument(
-    "--y-scale", "-y",
-    type = str,
-    default = 'log',
-    help = "Choose 'log' (default) or y-scale for likelihood plot"
-)
-parser.add_argument(
-    "--max-count-limit", "-c",
-    type = int,
-    help = "Choose maximum count for likelihood plot limits"
-)
-parser.add_argument(
-    "--min-count-limit", "-C",
-    type = int,
-    help = "Choose minimum count for likelihood plot limits"
-)
-parser.add_argument(
-    "--name", "-n",
-    type = str,
-    help = "Choose name for likelihood plot"
 )
 parser.add_argument(
     "--data-set-include-search-strings", "-d",
