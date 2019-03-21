a85bc4e1cc78ab983884531c030dc8b5b4722565
Christopher Heje Grønbech <chgroenbech@gmail.com>
2019-03-21 13:31:41 +0100

    Clean up Lomax distribution module

--- scvae/distributions/lomax.py
+++ scvae/distributions/lomax.py
@@ -11,11 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
+# ==========================================================================
 
 # ======================================================================== #
-# 
-# Copyright (c) 2017 - 2018 scVAE authors
+#
+# Copyright (c) 2017 - 2019 scVAE authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 # ======================================================================== #
 
 """The Lomax distribution class."""
@@ -38,7 +38,6 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
@@ -47,240 +46,217 @@ from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import check_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
-from tensorflow.python.ops import nn
-from tensorflow.python.ops import random_ops
-from tensorflow.python.ops.distributions import distribution
-from tensorflow.python.ops.distributions import kullback_leibler
-from tensorflow.python.ops.distributions import util as distribution_util
-
-
-__all__ = [
-    "Lomax",
-    "LomaxWithSoftplusConcentrationScale",
-]
+from tensorflow_probability.python.distributions import distribution
+from tensorflow_probability.python.internal import distribution_util
+from tensorflow_probability.python.internal import reparameterization
 
 
 class Lomax(distribution.Distribution):
-  """Lomax distribution.
+    """Lomax distribution.
 
-  The Lomax distribution is defined over positive real numbers and zero using
-  parameters `concentration` (aka "alpha") and `scale` (aka "lambda").
+    The Lomax distribution is defined over positive real numbers and zero using
+    parameters `concentration` (aka "alpha") and `scale` (aka "lambda").
 
-  #### Mathematical Details
+    #### Mathematical Details
 
-  The probability density function (pdf) is,
+    The probability density function (pdf) is,
 
-  ```none
-  pdf(x; alpha, lambda, x >= 0) = (1 + x / lambda)**(-(alpha + 1)) / Z
-  Z = lambda / alpha
-  ```
+    ```none
+    pdf(x; alpha, lambda, x >= 0) = (1 + x / lambda)**(-(alpha + 1)) / Z
+    Z = lambda / alpha
+    ```
 
-  where:
+    where:
 
-  * `concentration = alpha`, `alpha > 0`,
-  * `scale = lambda`, `lambda > 0`, and,
-  * `Z` is the normalizsing constant.
+    * `concentration = alpha`, `alpha > 0`,
+    * `scale = lambda`, `lambda > 0`, and,
+    * `Z` is the normalising constant.
 
-  The cumulative density function (cdf) is,
+    The cumulative density function (cdf) is,
 
-  ```none
-  cdf(x; alpha, lambda, x >= 0) = 1 - (1 + x / lambda)**(-alpha)
-  ```
+    ```none
+    cdf(x; alpha, lambda, x >= 0) = 1 - (1 + x / lambda)**(-alpha)
+    ```
 
-  Distribution parameters are automatically broadcast in all functions; see
-  examples for details.
+    Distribution parameters are automatically broadcast in all functions; see
+    examples for details.
 
-  #### Examples
+    #### Examples
 
-  ```python
-  dist = Lomax(concentration=3.0, scale=2.0)
-  dist2 = Lomax(concentration=[3.0, 4.0], scale=[2.0, 3.0])
-  ```
+    ```python
+    dist = Lomax(concentration=3.0, scale=2.0)
+    dist2 = Lomax(concentration=[3.0, 4.0], scale=[2.0, 3.0])
+    ```
 
-  """
-
-  def __init__(self,
-               concentration,
-               scale,
-               validate_args=False,
-               allow_nan_stats=True,
-               name="Lomax"):
-    """Construct Lomax with `concentration` and `scale` parameters.
-
-    The parameters `concentration` and `scale` must be shaped in a way that
-    supports broadcasting (e.g. `concentration + scale` is a valid operation).
-
-    Args:
-      concentration: Floating point tensor, the concentration params of the
-        distribution(s). Must contain only positive values.
-      scale: Floating point tensor, the inverse scale params of the
-        distribution(s). Must contain only positive values.
-      validate_args: Python `bool`, default `False`. When `True` distribution
-        parameters are checked for validity despite possibly degrading runtime
-        performance. When `False` invalid inputs may silently render incorrect
-        outputs.
-      allow_nan_stats: Python `bool`, default `True`. When `True`, statistics
-        (e.g., mean, mode, variance) use the value "`NaN`" to indicate the
-        result is undefined. When `False`, an exception is raised if one or
-        more of the statistic's batch members are undefined.
-      name: Python `str` name prefixed to Ops created by this class.
-
-    Raises:
-      TypeError: if `concentration` and `scale` are different dtypes.
     """
-    parameters = locals()
-    with ops.name_scope(name, values=[concentration, scale]):
-      with ops.control_dependencies([
-          check_ops.assert_positive(concentration),
-          check_ops.assert_positive(scale),
-      ] if validate_args else []):
-        self._concentration = array_ops.identity(
-            concentration, name="concentration")
-        self._scale = array_ops.identity(scale, name="scale")
-        check_ops.assert_same_float_dtype(
-            [self._concentration, self._scale])
-    super(Lomax, self).__init__(
-        dtype=self._concentration.dtype,
-        validate_args=validate_args,
-        allow_nan_stats=allow_nan_stats,
-        reparameterization_type=distribution.NOT_REPARAMETERIZED,
-        parameters=parameters,
-        graph_parents=[self._concentration,
-                       self._scale],
-        name=name)
 
-  @staticmethod
-  def _param_shapes(sample_shape):
-    return dict(
-        zip(("concentration", "scale"), ([ops.convert_to_tensor(
-            sample_shape, dtype=dtypes.int32)] * 2)))
+    def __init__(self,
+                 concentration,
+                 scale,
+                 validate_args=False,
+                 allow_nan_stats=True,
+                 name="Lomax"):
+        """Construct Lomax with `concentration` and `scale` parameters.
 
-  @property
-  def concentration(self):
-    """Concentration parameter."""
-    return self._concentration
+        The parameters `concentration` and `scale` must be shaped in a way that
+        supports broadcasting (e.g. `concentration + scale` is a valid
+        operation).
 
-  @property
-  def scale(self):
-    """Scale parameter."""
-    return self._scale
+        Args:
+            concentration: Floating point tensor, the concentration params of
+                the distribution(s). Must contain only positive values.
+            scale: Floating point tensor, the inverse scale params of the
+                distribution(s). Must contain only positive values.
+            validate_args: Python `bool`, default `False`. When `True`
+                distribution parameters are checked for validity despite
+                possibly degrading runtime performance. When `False` invalid
+                inputs may silently render incorrect outputs.
+            allow_nan_stats: Python `bool`, default `True`. When `True`,
+                statistics (e.g., mean, mode, variance) use the value "`NaN`"
+                to indicate the result is undefined. When `False`, an
+                exception is raised if one or more of the statistic's batch
+                members are undefined.
+            name: Python `str` name prefixed to Ops created by this class.
 
-  def _batch_shape_tensor(self):
-    return array_ops.broadcast_dynamic_shape(
-        array_ops.shape(self.concentration),
-        array_ops.shape(self.scale))
+        Raises:
+            TypeError: if `concentration` and `scale` are different dtypes.
+        """
+        parameters = locals()
+        with ops.name_scope(name, values=[concentration, scale]):
+            with ops.control_dependencies([
+                check_ops.assert_positive(concentration),
+                check_ops.assert_positive(scale),
+            ] if validate_args else []):
+                self._concentration = array_ops.identity(
+                    concentration, name="concentration")
+                self._scale = array_ops.identity(scale, name="scale")
+                check_ops.assert_same_float_dtype(
+                    [self._concentration, self._scale])
+        super(Lomax, self).__init__(
+            dtype=self._concentration.dtype,
+            validate_args=validate_args,
+            allow_nan_stats=allow_nan_stats,
+            reparameterization_type=reparameterization.NOT_REPARAMETERIZED,
+            parameters=parameters,
+            graph_parents=[self._concentration,
+                           self._scale],
+            name=name)
 
-  def _batch_shape(self):
-    return array_ops.broadcast_static_shape(
-        self.concentration.get_shape(),
-        self.scale.get_shape())
+    @staticmethod
+    def _param_shapes(sample_shape):
+        return dict(
+            zip(("concentration", "scale"), ([ops.convert_to_tensor(
+                sample_shape, dtype=dtypes.int32)] * 2)))
 
-  def _event_shape_tensor(self):
-    return constant_op.constant([], dtype=dtypes.int32)
+    @property
+    def concentration(self):
+        """Concentration parameter."""
+        return self._concentration
 
-  def _event_shape(self):
-    return tensor_shape.scalar()
+    @property
+    def scale(self):
+        """Scale parameter."""
+        return self._scale
 
-  def _log_prob(self, x):
-    return self._log_unnormalized_prob(x) - self._log_normalization()
+    def _batch_shape_tensor(self):
+        return array_ops.broadcast_dynamic_shape(
+            array_ops.shape(self.concentration),
+            array_ops.shape(self.scale))
 
-  def _prob(self, x):
-    return math_ops.exp(self._log_prob(x))
+    def _batch_shape(self):
+        return array_ops.broadcast_static_shape(
+            self.concentration.get_shape(),
+            self.scale.get_shape())
 
-  def _log_cdf(self, x):
-    return math_ops.log(self._cdf(x))
+    def _event_shape_tensor(self):
+        return constant_op.constant([], dtype=dtypes.int32)
 
-  def _cdf(self, x):
-    x = self._maybe_assert_valid_sample(x)
-    return 1 - math_ops.pow(1 + x / self.scale, - self.concentration)
+    def _event_shape(self):
+        return tensor_shape.scalar()
 
-  def _log_unnormalized_prob(self, x):
-    x = self._maybe_assert_valid_sample(x)
-    return - (self.concentration + 1.) * math_ops.log(1 + x / self.scale)
+    def _log_prob(self, x):
+        return self._log_unnormalized_prob(x) - self._log_normalization()
 
-  def _log_normalization(self):
-    return math_ops.log(self.scale) - math_ops.log(self.concentration)
+    def _prob(self, x):
+        return math_ops.exp(self._log_prob(x))
 
-  @distribution_util.AppendDocstring(
-      """The mean of a Lomax distribution is only defined for
-      `concentration > 1`, and `NaN` otherwise. If `self.allow_nan_stats` is
-      `False`, an exception will be raised rather than returning `NaN`.""")
-  def _mean(self):
-    mean = self.scale / (self.concentration - 1)
-    if self.allow_nan_stats:
-      nan = array_ops.fill(
-          self.batch_shape_tensor(),
-          np.array(np.nan, dtype=self.dtype.as_numpy_dtype()),
-          name="nan")
-      return array_ops.where(self.concentration > 1., mean, nan)
-    else:
-      return control_flow_ops.with_dependencies([
-          check_ops.assert_less(
-              array_ops.ones([], self.dtype),
-              self.concentration,
-              message="mean not defined when any concentration <= 1"),
-          ], mode)
+    def _log_cdf(self, x):
+        return math_ops.log(self._cdf(x))
 
-  @distribution_util.AppendDocstring(
-      """The variance of a Lomax distribution is only defined for
-      `concentration > 1`, and `NaN` otherwise. If `self.allow_nan_stats` is
-      `False`, an exception will be raised rather than returning `NaN`.""")
-  def _variance(self):
-    variance = math_ops.square(self.scale) * (self.concentration - 1) \
-      / (math_ops.square(self.concentration - 1) * (self.concentration - 2))
-    if self.allow_nan_stats:
-      nan = array_ops.fill(
-          self.batch_shape_tensor(),
-          np.array(np.nan, dtype=self.dtype.as_numpy_dtype()),
-          name="nan")
-      inf = array_ops.fill(
-          self.batch_shape_tensor(),
-          np.array(np.inf, dtype=self.dtype.as_numpy_dtype()),
-          name="inf")
-      return array_ops.where(self.concentration > 2., variance, #nan)
-        array_ops.where(self.concentration > 1., inf, nan))
-    else:
-      return control_flow_ops.with_dependencies([
-          check_ops.assert_less(
-              array_ops.ones([], self.dtype),
-              self.concentration,
-              message="standard deviation not defined when any concentration <= 1"),
-          ], mode)
+    def _cdf(self, x):
+        x = self._maybe_assert_valid_sample(x)
+        return 1 - math_ops.pow(1 + x / self.scale, - self.concentration)
 
-  def _stddev(self):
-    return math_ops.sqrt(self._variance())
+    def _log_unnormalized_prob(self, x):
+        x = self._maybe_assert_valid_sample(x)
+        return - (self.concentration + 1.) * math_ops.log(1 + x / self.scale)
 
-  def _mode(self):
-    return array_ops.fill(
-      self.batch_shape_tensor(),
-      np.array(0, dtype=self.dtype.as_numpy_dtype()),
-      name="zero")
+    def _log_normalization(self):
+        return math_ops.log(self.scale) - math_ops.log(self.concentration)
 
-  def _maybe_assert_valid_sample(self, x):
-    check_ops.assert_same_float_dtype(tensors=[x], dtype=self.dtype)
-    if not self.validate_args:
-      return x
-    return control_flow_ops.with_dependencies([
-        check_ops.assert_non_negative(x),
-    ], x)
+    @distribution_util.AppendDocstring(
+        """The mean of a Lomax distribution is only defined for
+        `concentration > 1`, and `NaN` otherwise. If `self.allow_nan_stats` is
+        `False`, an exception will be raised rather than returning `NaN`.""")
+    def _mean(self):
+        mean = self.scale / (self.concentration - 1)
+        if self.allow_nan_stats:
+            nan = array_ops.fill(
+                self.batch_shape_tensor(),
+                np.array(np.nan, dtype=self.dtype.as_numpy_dtype()),
+                name="nan")
+            return array_ops.where(self.concentration > 1., mean, nan)
+        else:
+            return control_flow_ops.with_dependencies([
+                check_ops.assert_less(
+                    array_ops.ones([], self.dtype),
+                    self.concentration,
+                    message="mean not defined when any concentration <= 1"
+                ),
+            ], mean)
 
+    @distribution_util.AppendDocstring(
+        """The variance of a Lomax distribution is only defined for
+        `concentration > 1`, and `NaN` otherwise. If `self.allow_nan_stats` is
+        `False`, an exception will be raised rather than returning `NaN`.""")
+    def _variance(self):
+        variance = (math_ops.square(self.scale) * (self.concentration - 1)
+                    / (math_ops.square(self.concentration - 1)
+                    * (self.concentration - 2)))
+        if self.allow_nan_stats:
+            nan = array_ops.fill(
+                    self.batch_shape_tensor(),
+                    np.array(np.nan, dtype=self.dtype.as_numpy_dtype()),
+                    name="nan")
+            inf = array_ops.fill(
+                    self.batch_shape_tensor(),
+                    np.array(np.inf, dtype=self.dtype.as_numpy_dtype()),
+                    name="inf")
+            return array_ops.where(
+                self.concentration > 2., variance, array_ops.where(
+                    self.concentration > 1., inf, nan))
+        else:
+            return control_flow_ops.with_dependencies([
+                check_ops.assert_less(
+                    array_ops.ones([], self.dtype),
+                    self.concentration,
+                    message="variance not defined when any concentration <= 1"
+                ),
+            ], variance)
 
-class LomaxWithSoftplusConcentrationScale(Lomax):
-  """`Lomax` with softplus of `concentration` and `scale`."""
+    def _stddev(self):
+        return math_ops.sqrt(self._variance())
 
-  def __init__(self,
-               concentration,
-               scale,
-               validate_args=False,
-               allow_nan_stats=True,
-               name="LomaxWithSoftplusConcentrationScale"):
-    parameters = locals()
-    with ops.name_scope(name, values=[concentration, scale]):
-      super(LomaxWithSoftplusConcentrationScale, self).__init__(
-          concentration=nn.softplus(concentration,
-                                    name="softplus_concentration"),
-          scale=nn.softplus(scale, name="softplus_scale"),
-          validate_args=validate_args,
-          allow_nan_stats=allow_nan_stats,
-          name=name)
-    self._parameters = parameters
+    def _mode(self):
+        return array_ops.fill(
+            self.batch_shape_tensor(),
+            np.array(0, dtype=self.dtype.as_numpy_dtype()),
+            name="zero")
+
+    def _maybe_assert_valid_sample(self, x):
+        check_ops.assert_same_float_dtype(tensors=[x], dtype=self.dtype)
+        if not self.validate_args:
+            return x
+        return control_flow_ops.with_dependencies([
+                check_ops.assert_non_negative(x),
+        ], x)
