264eb0eb0ead0aef23f24a1d560c2f6007d00201
Christopher Heje Grønbech <chgroenbech@gmail.com>
2019-05-18 20:39:14 +0200

    Correct GitHub URL

--- scvae/__init__.py
+++ scvae/__init__.py
@@ -2,7 +2,7 @@ from scvae.__version__ import __version__
 
 __title__ = "scvae"
 __description__ = "Model single-cell transcript counts using deep learning."
-__url__ = "http://github.com/chgroenbech/scvae"
+__url__ = "http://github.com/scvae/scvae"
 __author__ = "Christopher Heje Grønbech, Maximillian Fornitz Vording"
 __maintainer__ = "Christopher Heje Grønbech"
 __email__ = "christopher.heje.groenbech@regionh.dk"
