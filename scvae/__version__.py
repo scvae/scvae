72f97d40a32c680ca874d1e232a3b71e8388e0ec
Christopher Heje Grønbech <chgroenbech@gmail.com>
2019-11-12 12:36:24 +0100

    Update version to 2.1.0dev

--- scvae/__version__.py
+++ scvae/__version__.py
@@ -1,6 +1,6 @@
 MAJOR_VERSION = "2"
-MINOR_VERSION = "0"
-PATCH_VERSION = "1"
+MINOR_VERSION = "1"
+PATCH_VERSION = "0"
 VERSION_SUFFIX = "dev"
 
 VERSION = (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
@@ -8,4 +8,4 @@ VERSION = (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 __version__ = ".".join(map(str, VERSION))
 
 if VERSION_SUFFIX:
-    __version__ = "{}-{}".format(__version__, VERSION_SUFFIX)
+    __version__ = __version__ + VERSION_SUFFIX
