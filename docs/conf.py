096fbe30fa7651f03249303a94856bbd4f5ee5cf
Christopher Heje Grønbech <chgroenbech@gmail.com>
2019-11-13 10:40:33 +0100

    Simplify documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
 import os
 import sys
 
@@ -17,31 +5,29 @@ sys.path.insert(0, os.path.abspath(".."))
 
 import scvae
 
-# -- Project information -----------------------------------------------------
+# Project information
 
 project = scvae.__title__
 copyright = scvae.__copyright__
 author = scvae.__author__
-
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
 version = scvae.__version__
-# The full version, including alpha/beta/rc tags.
 release = scvae.__version__
 
-# -- General configuration ---------------------------------------------------
+# General configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.napoleon"
 ]
+templates_path = ["_templates"]
+source_suffix = {
+    ".rst": "restructuredtext"
+}
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+default_role = "py:obj"
+
+# autodoc options
 
 autodoc_member_order = "bysource"
 autodoc_mock_imports = [
@@ -58,6 +44,10 @@ autodoc_mock_imports = [
     "tensorflow_probability"
 ]
 
+# HTML options
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
 html_theme_options = {
     "github_user": "scvae",
     "github_repo": "scvae",
@@ -65,7 +55,6 @@ html_theme_options = {
     "description": "Single-cell variational auto-encoders",
     "show_powered_by": False
 }
-
 html_sidebars = {
     "**": [
         "about.html",
@@ -73,31 +62,3 @@ html_sidebars = {
         "searchbox.html",
     ]
 }
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-source_suffix = {
-    ".rst": "restructuredtext"
-}
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-default_role = "py:obj"
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
