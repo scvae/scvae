79aa8e1573888b52a2a99ebea04c7fb9143b23a5
Christopher Heje Grønbech <chgroenbech@gmail.com>
2020-06-29 10:45:33 +0200

    Update copyright end year in source files

--- scvae/distributions/zero_inflated.py
+++ scvae/distributions/zero_inflated.py
@@ -15,7 +15,7 @@
 
 # ======================================================================== #
 #
-# Copyright (c) 2017 - 2019 scVAE authors
+# Copyright (c) 2017 - 2020 scVAE authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
